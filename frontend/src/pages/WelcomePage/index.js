1bdde4c9a676369da2bacf3f10d427a3eb5a4205
Luke Yamasaki <lukeyamasaki@gmail.com>
2022-03-03 22:21:46 -0700

    Adding welcome page styling

--- frontend/src/pages/WelcomePage/index.js
+++ frontend/src/pages/WelcomePage/index.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { useSelector } from 'react-redux';
-import { Redirect } from 'react-router-dom';
+import { Redirect, Link, NavLink } from 'react-router-dom';
 import styled from 'styled-components';
 import LoginForm from '../../components/Forms/UserForms/LoginForm';
 import SignupForm from '../../components/Forms/UserForms/SignupForm';
@@ -15,12 +15,15 @@ const WelcomeWrapper= styled.div`
     align-items: center;
 `;
 
-const WelcomeBackground = styled.div`
+const WelcomeSection = styled.section`
     width: 100vw;
     height: 95vh;
     display: flex;
+    flex-direction: column;
     justify-content: center;
     align-items: center;
+    color: white;
+    font-family: sans-serif;
 `;
 
 const WelcomeNav = styled.nav`
@@ -37,10 +40,19 @@ const LogoButton = styled.button`
     height: 75%;
 `
 
+const Footer = styled.footer`
+    display: flex;
+    flex-direction: row;
+    width: 100vw;
+    height: 5vh;
+    background-color: black;
+    justify-content: space-around;
+    align-items: center;
+`
+
 
 const WelcomePage = () => {
     const sessionUser = useSelector(state => state.session.user);
-    const [userAction, setUserAction] = useState('login')
 
     if (sessionUser) return (
       <Redirect to="/" />
@@ -50,8 +62,20 @@ const WelcomePage = () => {
         <WelcomeWrapper className='wlc-background'>
             <WelcomeNav>
                 <LogoButton className='wlc-logo-btn' />
+                <div>search bar</div>
+                <NavLink to='/login'>Log In</NavLink>
+                <NavLink to='/signup'>Sign Up</NavLink>
             </WelcomeNav>
-            <WelcomeBackground  />
+            <WelcomeSection>
+                <h1 style={{fontSize: '50px'}}>Find your inspiration.</h1>
+                <h3>Join the Shuttr community, home to high resolution photos and countless groups.</h3>
+                <Link className='wlc-start-btn' to='/signup'>
+                    Start for free
+                </Link>
+            </WelcomeSection>
+            <h5>Photo title</h5>
+            <h5>by uploader</h5>
+            <Footer />
         </WelcomeWrapper>
 
 
