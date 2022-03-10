117164fb3b8d8f674250d7ddce557f8adf3b213f
Luke Yamasaki <lukeyamasaki@gmail.com>
2022-03-09 22:18:20 -0700

    Adding 404 page

--- frontend/src/pages/404Page/index.js
+++ frontend/src/pages/404Page/index.js
@@ -14,28 +14,129 @@ const NotFoundPage = () => {
     })
 
     return (
-        <div>
+        <div className={styles.notFoundWrapper}>
             {loggedIn &&
                 <Navbar></Navbar>
             }
             <h1>404 Not Found</h1>
-            <svg viewbox="0 0 2500 100" className={styles.svg}>
+            <svg viewbox="0 0 2500 2000" className={styles.svg}>
                 <defs>
-                <pattern id="img1" patternUnits="userSpaceOnUse" width="100" height="200">
+                <pattern id="img1" patternUnits="userSpaceOnUse" width="100" height="100">
                     <image href={sessionUser.profileImageUrl} x="0" y="0" width="100" height="100" />
                 </pattern>
                 </defs>
                 <path fill="url(#img1)" stroke="red"
-                    d="M 0,100
+                    d="
+                    M 0,100
                     Q 15,15 500,50
                     t 500,0
                     Q 1000,25 1500,50
                     t 500,0
                     Q 2000,25 2500,50
                     t 500,0
-                    L 3000,100 3000,200
-                    L 3000,200 0, 200
-                    L 0,200 0,100 z
+                    L 3000,200
+                    t -3000,200
+                    z
+                    " />
+            </svg>
+            <svg viewbox="0 0 2500 2000" className={styles.svg}>
+                <defs>
+                <pattern id="img1" patternUnits="userSpaceOnUse" width="100" height="200">
+                    <image href={sessionUser.profileImageUrl} x="0" y="0" width="100" height="100" />
+                </pattern>
+                </defs>
+                <path fill="url(#img1)" stroke="red"
+                    d="
+                    M 0,100
+                    Q 15,15 500,50
+                    t 500,0
+                    Q 1000,25 1500,50
+                    t 500,0
+                    Q 2000,25 2500,50
+                    t 500,0
+                    L 3000,200
+                    t -3000,200
+                    z
+                    " />
+            </svg>
+            <svg viewbox="0 0 2500 2000" className={styles.svg}>
+                <defs>
+                <pattern id="img1" patternUnits="userSpaceOnUse" width="100" height="200">
+                    <image href={sessionUser.profileImageUrl} x="0" y="0" width="100" height="100" />
+                </pattern>
+                </defs>
+                <path fill="url(#img1)" stroke="red"
+                    d="
+                    M 0,100
+                    Q 15,15 500,50
+                    t 500,0
+                    Q 1000,25 1500,50
+                    t 500,0
+                    Q 2000,25 2500,50
+                    t 500,0
+                    L 3000,200
+                    t -3000,200
+                    z
+                    " />
+            </svg>
+            <svg viewbox="0 0 2500 2000" className={styles.svg}>
+                <defs>
+                <pattern id="img1" patternUnits="userSpaceOnUse" width="100" height="200">
+                    <image href={sessionUser.profileImageUrl} x="0" y="0" width="100" height="100" />
+                </pattern>
+                </defs>
+                <path fill="url(#img1)" stroke="red"
+                    d="
+                    M 0,100
+                    Q 15,15 500,50
+                    t 500,0
+                    Q 1000,25 1500,50
+                    t 500,0
+                    Q 2000,25 2500,50
+                    t 500,0
+                    L 3000,200
+                    t -3000,200
+                    z
+                    " />
+            </svg>
+            <svg viewbox="0 0 2500 2000" className={styles.svg}>
+                <defs>
+                <pattern id="img1" patternUnits="userSpaceOnUse" width="100" height="200">
+                    <image href={sessionUser.profileImageUrl} x="0" y="0" width="100" height="100" />
+                </pattern>
+                </defs>
+                <path fill="url(#img1)" stroke="red"
+                    d="
+                    M 0,100
+                    Q 15,15 500,50
+                    t 500,0
+                    Q 1000,25 1500,50
+                    t 500,0
+                    Q 2000,25 2500,50
+                    t 500,0
+                    L 3000,200
+                    t -3000,200
+                    z
+                    " />
+            </svg>
+            <svg viewbox="0 0 2500 2000" className={styles.svg}>
+                <defs>
+                <pattern id="img1" patternUnits="userSpaceOnUse" width="100" height="200">
+                    <image href={sessionUser.profileImageUrl} x="0" y="0" width="100" height="100" />
+                </pattern>
+                </defs>
+                <path fill="url(#img1)" stroke="red"
+                    d="
+                    M 0,100
+                    Q 15,15 500,50
+                    t 500,0
+                    Q 1000,25 1500,50
+                    t 500,0
+                    Q 2000,25 2500,50
+                    t 500,0
+                    L 3000,200
+                    t -3000,200
+                    z
                     " />
             </svg>
             {/* <img src={sessionUser.profileImageUrl} style={{width: '1000px', height: '750px'}}></img> */}
