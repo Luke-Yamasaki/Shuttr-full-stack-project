fbfb836cd9d9166d64180f6c086bdb8d3c4659e9
Luke Yamasaki <lukeyamasaki@gmail.com>
2022-03-12 22:31:33 -0700

    Added sorting to homepage

--- frontend/src/pages/Homepage/index.js
+++ frontend/src/pages/Homepage/index.js
@@ -9,24 +9,12 @@ import { getImages } from '../../store/images';
 import styled from 'styled-components';
 import styles from './Homepage.module.css';
 
-const HomeWrapper = styled.div`
-    width: 100vw;
-    height: 100vh;
-    display: flex;
-    flex-direction: column;
-    justify-content: space-between;
-    align-items: center;
-    overflow-y: scroll;
-    overflow-x: hidden;
-`;
-
 const HomeFeed = styled.div`
     width: 100vw;
     height: 95vh;
     display: flex;
     flex-direction: row;
     justify-content: space-around;
-    align-items: flex-start;
 `;
 
 const ActivityBox = styled.div`
@@ -40,8 +28,7 @@ const ActivityBox = styled.div`
 const ImageSection = styled.section`
     width: 47vw;
     height: 100vh;
-    background-color: white;
-    border: 1px solid grey;
+    background-color: green;
     border-radius: 0.01rem;
     display:flex;
     flex-direction: column;
@@ -56,12 +43,26 @@ const Homepage = () => {
     const usersArr = Object.values(usersObj);
     const imagesObj = useSelector(state => state.imagesState.images);
     const imagesArr = Object.values(imagesObj);
+    const imagesArrCopy = [...imagesArr];
+    const reversedImagesArr = imagesArrCopy.reverse();
     const [activity, setActivity] = useState('All Activity');
     const [loaded, setLoaded] = useState(false);
+    const [showButton, setShowButton] = useState(true)
+    const [more, setMore] = useState('hidden');
+
+    useEffect(() => {
+        setActivity('All Activity');
+    },[])
 
     useEffect(() => {
         dispatch(restoreUser()).then(() => dispatch(getUsers()).then(() => dispatch(getImages()).then(() => setLoaded(true))));
     },[dispatch])
+
+    useEffect(() => {
+        if(activity === 'Newest') {
+            dispatch(getImages()).then((res) => console.log(res))
+        }
+    },[activity])
 
     const handleLogout = () => {
         if (sessionUser) {
@@ -71,25 +72,49 @@ const Homepage = () => {
         }
     };
 
+    const handleMore = (e) => {
+        e.preventDefault();
+        let div = document.getElementsByClassName('wrapper')[0];
+        if(more === 'hidden') {
+            setShowButton(false);
+            setMore('scroll');
+            div.style.overflowY = more;
+        }
+    }
+
     if (!sessionUser)  {
         return <Redirect to="/welcome" />
     }
 
+    const handleSorting = (e) => {
+        e.preventDefault();
+        const selectBox = document.getElementsByTagName('select')[0];
+        const value = selectBox.options[selectBox.selectedIndex].value;
+        setActivity(value);
+    }
+
+    // const handleSelectOld = (e) => {
+    //     e.preventDefault();
+    //     if(activity === 'Newest' && e.target.value === 'Oldest') {
+    //         setActivity(e.target.value)
+    //     }
+    // }
+
     return loaded && (
-        <HomeWrapper>
+        <div className={styles.wrapper} style={{overflowY: `${more}`}}>
             <Navbar/>
             <HomeFeed>
                 <ActivityBox>
-                    <select className={styles.selectActivity} onBlur={(e) => e.target.style.border = 'none'}>
-                        <option>All Activity</option>
-                        <option>People</option>
-                        <option>Images</option>
+                    <select onChange={handleSorting} name='sorting' className={styles.selectActivity} onBlur={(e) => e.target.style.border = 'none'}>
+                        <option value=''>--  Activity  --</option>
+                        <option value='Oldest'>Oldest</option>
+                        <option value='Newest'>Newest</option>
                     </select>
                     <ImageSection>
                         <h5>People to follow</h5>
 
                         <div className={styles.imageCardWrapper}>
-                            {imagesArr.map(image =>
+                            {activity === "Oldest" || activity === "All Activity" ? imagesArr.map(image =>
                                 <div key={image.id} className={styles.imageCardBox}>
                                     <div className={styles.imageDiv} style={{backgroundImage: `url(${image.imageUrl})`}}></div>
                                     <div className={styles.userInfoBox}>
@@ -100,11 +125,24 @@ const Homepage = () => {
                                         </div>
                                     </div>
                                 </div>
-                            )}
+                            )
+                            :
+                            reversedImagesArr.map(image =>
+                                <div key={image.id} className={styles.imageCardBox}>
+                                    <div className={styles.imageDiv} style={{backgroundImage: `url(${image.imageUrl})`}}></div>
+                                    <div className={styles.userInfoBox}>
+                                        <img className={styles.userIcon} src={sessionUser.profileImageUrl}></img>
+                                        <div className={styles.userDetails}>
+                                            <Link to={`/users/${image.userId}`} className={styles.userLink}>{usersObj[image.userId].firstName} {usersObj[image.userId].lastName}</Link>
+                                            <p className={styles.userInfoText}>{usersObj[image.userId].username}</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            )
+                        }
                         </div>
 
-
-                        <button>View more</button>
+                        {showButton && <div className={styles.moreDiv} onClick={(e) => handleMore(e)}><button type='button' onClick={handleMore} className={styles.moreBtn}>View more</button></div>}
                     </ImageSection>
                 </ActivityBox>
                 {/* <aside>
@@ -143,7 +181,7 @@ const Homepage = () => {
                         </div>
                     </aside> */}
             </HomeFeed>
-        </HomeWrapper>
+        </div>
     )
 }
 
